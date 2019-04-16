2976c765ebb41f6a31102c3582b3c8bfcf05db00
vagrant <vagrant@homestead>
2019-04-16 14:58:54 +0000

    编辑个人资料

---
+++ database/migrations/2019_04_16_225113_add_avatar_and_introduction_to_users_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddAvatarAndIntroductionToUsersTable extends Migration {
+	/**
+	 * 执行迁移
+	 *
+	 * @return void
+	 */
+	public function up() {
+		Schema::table('users', function (Blueprint $table) {
+			$table->string('avatar')->nullable();
+			$table->string('introduction')->nullable();
+		});
+	}
+
+	/**
+	 * 回滚迁移
+	 *
+	 * @return void
+	 */
+	public function down() {
+		Schema::table('users', function (Blueprint $table) {
+			$table->dropColumn('avatar');
+			$table->dropColumn('introduction');
+		});
+	}
+}
