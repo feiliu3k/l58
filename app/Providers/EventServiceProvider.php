bce69edb4813691ac03ed4067688f76c0d03adf7
vagrant <vagrant@homestead>
2019-04-16 14:38:19 +0000

    找回密码成功消息提示

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,33 +2,34 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Event;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Event;
 
-class EventServiceProvider extends ServiceProvider
-{
-    /**
-     * The event listener mappings for the application.
-     *
-     * @var array
-     */
-    protected $listen = [
-        Registered::class => [
-            SendEmailVerificationNotification::class,
-        ],
-    ];
+class EventServiceProvider extends ServiceProvider {
+	/**
+	 * The event listener mappings for the application.
+	 *
+	 * @var array
+	 */
+	protected $listen = [
+		Registered::class => [
+			SendEmailVerificationNotification::class,
+		],
+		\Illuminate\Auth\Events\Verified::class => [
+			\App\Listeners\EmailVerified::class,
+		],
+	];
 
-    /**
-     * Register any events for your application.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        parent::boot();
+	/**
+	 * Register any events for your application.
+	 *
+	 * @return void
+	 */
+	public function boot() {
+		parent::boot();
 
-        //
-    }
+		//
+	}
 }
