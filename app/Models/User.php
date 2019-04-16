e42de338b19874071627e1e48acac489bd0c8e32
vagrant <vagrant@homestead>
2019-04-16 14:17:55 +0000

    邮箱认证

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,36 +2,19 @@
 
 namespace App\Models;
 
+use Illuminate\Auth\MustVerifyEmail as MustVerifyEmailTrait;
+use Illuminate\Contracts\Auth\MustVerifyEmail as MustVerifyEmailContract;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable {
-	use Notifiable;
+class User extends Authenticatable implements MustVerifyEmailContract {
+	use Notifiable, MustVerifyEmailTrait;
 
-	/**
-	 * The attributes that are mass assignable.
-	 *
-	 * @var array
-	 */
 	protected $fillable = [
 		'name', 'email', 'password',
 	];
 
-	/**
-	 * The attributes that should be hidden for arrays.
-	 *
-	 * @var array
-	 */
 	protected $hidden = [
 		'password', 'remember_token',
-	];
-
-	/**
-	 * The attributes that should be cast to native types.
-	 *
-	 * @var array
-	 */
-	protected $casts = [
-		'email_verified_at' => 'datetime',
 	];
 }
